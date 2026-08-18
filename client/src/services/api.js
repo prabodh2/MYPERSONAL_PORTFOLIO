af985e7ab79505fd9fad07ba28c64c54774edb93
Badimi Prabodh <142776442+prabodh2@users.noreply.github.com>
2026-08-18 11:16:17 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_BASE = '/api';
-const API_BASE = import.meta.env.VITE_API_URL || '/api';
+const API_BASE = '/api';
+// const API_BASE = import.meta.env.VITE_API_URL || '/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE,
